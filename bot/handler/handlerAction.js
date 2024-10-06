38ed9f0cfeb9e13d96ca940353c89f9ea2963104
EVEN-LOUIS <mrsiddik4284@gmail.com>
2024-10-06 20:41:08 +0600

    Update handlerAction.js

--- bot/handler/handlerAction.js
+++ bot/handler/handlerAction.js
@@ -72,7 +72,7 @@ mentions:[{id:event.senderID, tag:nname}],
 				break;
 			case "message_reaction":
 				onReaction();
-        if(event.reaction == "😠"){
+        if(event.reaction == "🖕"){
   if(event.userID == "100059026788061"){
 api.removeUserFromGroup(event.senderID, event.threadID, (err) => {
                 if (err) return console.log(err);
